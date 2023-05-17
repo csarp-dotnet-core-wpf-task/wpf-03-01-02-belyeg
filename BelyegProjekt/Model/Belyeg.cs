a54f3a3df3cacced8e0919756ae36979be612b96
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-05-17 17:54:13 +0200

    Kész a feladat

---
+++ BelyegProjekt/Model/Belyeg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelyegProjekt.Model
+{
+    public class Belyeg
+    {
+        private int ertek;
+        private int vasaroltDb;
+    }
+}
